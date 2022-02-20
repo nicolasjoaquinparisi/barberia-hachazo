0b0a70ed2d1fd6dbb610fb9d4c6bd114532c057d
Nicolás Joaquín Parisi <nicojp.1808@gmail.com>
2022-02-20 18:57:11 -0300

    GETs, PUT y DELETE agregados para la entidad Turn

--- controller/turnController.js
+++ controller/turnController.js
@@ -3,6 +3,7 @@ const Barber = require('../model/Barber')
 const Service = require('../model/Service')
 const Turn = require('../model/Turn')
 const { validationResult } = require('express-validator')
+const Person = require('../model/Person')
 
 exports.createTurn = async(req, res) => {
     const errors = validationResult(req)
@@ -54,7 +55,20 @@ exports.createTurn = async(req, res) => {
 
 exports.getTurns = async(req, res) => {
     try {
-        const turns = await Turn.findAll()
+        const turns = await Turn.findAll({
+            include: [
+                { model: Barber },
+                { model: Client },
+                { model: Service }
+            ],
+            attributes: {
+                exclude: [
+                    'client_id',
+                    'barber_id',
+                    'service_id'
+                ]
+            }
+        })
         res.json({turns})
     } catch (error) {
         console.log(error)
@@ -64,7 +78,27 @@ exports.getTurns = async(req, res) => {
 
 exports.getTurn = async(req, res) => {
     try {
+        const id = req.params.id
 
+        const turn = await Turn.findByPk(id, {
+            include: [
+                { model: Barber },
+                { model: Client },
+                { model: Service }
+            ],
+            attributes: {
+                exclude: [
+                    'client_id',
+                    'barber_id',
+                    'service_id'
+                ]
+            }
+        })
+        if (!turn) {
+            return res.status(400).send(`Turn not found`)
+        }
+
+        res.json({turn})
     } catch (error) {
         console.log(error)
         res.status(500).send('There was a server error')
@@ -79,6 +113,48 @@ exports.updateTurn = async(req, res) => {
 
     try {
 
+        const id = req.params.id
+
+        const turnToUpdate = await Turn.findByPk(id)
+        if (!turnToUpdate) {
+            return res.status(400).send(`Turn not found`)
+        }
+
+        const { date, idService, idBarber, idClient } = req.body
+
+        if (date <= new Date().getTime()) {
+            return res.status(400).send(`The date must be today or a future date`)
+        }
+
+        const service = await Service.findByPk(idService)
+        if (!service) {
+            return res.status(400).send(`Service not found`)
+        }
+
+        const barber = await Barber.findByPk(idBarber)
+        if (!barber) {
+            return res.status(400).send(`Barber not found`)
+        }
+
+        const client = await Client.findByPk(idClient)
+        if (!client) {
+            return res.status(400).send(`Client not found`)
+        }
+
+        const existTurnInTheSameDate = await Turn.findOne({ where: {date:date} })
+        if (existTurnInTheSameDate && existTurnInTheSameDate.id !== turnToUpdate.id) {
+            return res.status(400).send(`There is a turn in the same date`)
+        }
+
+        turnToUpdate.set({
+            date: date,
+            service_id: idService,
+            barber_id: idBarber,
+            client_id: idClient
+        })
+        await turnToUpdate.save()
+
+        res.status(200).send(`Turn updated`)
     } catch (error) {
         console.log(error)
         res.status(500).send('There was a server error')
